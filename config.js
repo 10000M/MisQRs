ebd375feab95214369681c6a574f6f5f97ee6b85
10000M <tics.desarrollo@libredepsicologia.edu.mx>
2024-09-02 14:25:24 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxUSz7C7pH4Y_QGT-p2dZRzYpTShyQn6AK46FFTFiiea7rHkSHo6J5UL5DdAXCOdEp63w/exec";
